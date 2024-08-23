4ef1dd9555b1fa8fa628e095a5bebc78f2983fdd
Theo Akande <ta722@ic.ac.uk>
2024-08-23 22:14:10 +0100

    feat: skeleton for object class

---
+++ src/Object.h
@@ -0,0 +1,54 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <SOIL2/SOIL2.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace std;
+
+#define numObjectVAOs 1
+#define numObjectVBOs 1
+
+struct Line {
+    float x1, y1;
+    float x2, y2;
+    float nx, ny;
+    float elasticity;
+};
+
+class Object
+{
+    private:
+        vector<glm::vec2> *vertices;
+        vector<Line> *edges;
+        float mass, scale;
+        float x, y;
+        float elasticity;
+        glm::vec4 colour;
+        GLuint bvao[numObjectVAOs];
+        GLuint bvbo[numObjectVBOs];
+        bool active;
+        void drawObject(void);
+
+        static GLuint objectShaderProgram;
+        static vector<Object *> objects;
+        static bool initialized;
+        static int screenWidth, screenHeight;
+    public:
+        int numEdges;
+        int numVertices;
+        Object(const char *path);
+        Object(vector<glm::vec2> *vertices);
+        void setMass(float mass);
+        void setScale(float scale);
+        void translate(float x, float y);
+        
+        static void initObjects(int screenWidth, int screenHeight);
+        static void update(void);
+};
