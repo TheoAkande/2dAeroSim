920b51a37ec06628b763a426cbab699af0f8fa8c
Theo Akande <ta722@ic.ac.uk>
2024-08-24 14:47:25 +0100

    feat: updated object shader

--- src/shaders/objectFrag.glsl
+++ src/shaders/objectFrag.glsl
@@ -3,9 +3,11 @@
 layout(location = 0) in vec2 vertexPosition;
 
 uniform float sf;
+uniform mat4 viewMat;
+uniform vec3 colourIn;
 
 out vec4 colour;
 
 void main() {
-    colour = vec4(1.0, 0.0, 0.0, 1.0);
+    colour = vec4(colourIn, 1.0);
 }
--- src/shaders/objectVert.glsl
+++ src/shaders/objectVert.glsl
@@ -4,6 +4,7 @@ layout (location = 0) in vec2 vertexPosition;
 
 uniform float sf;
 uniform mat4 viewMat;
+uniform vec3 colourIn;
 
 void main() {
     gl_Position = viewMat * vec4(vertexPosition.x / sf, vertexPosition.y / sf, 0.0, 1.0);
